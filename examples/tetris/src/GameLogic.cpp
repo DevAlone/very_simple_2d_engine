a6c1ef4c65eda626368840bb1a14b577c9980d06
DevAlone <dev@d3d.info>
2018-11-27 22:45:00 +0300

    tetris

--- examples/tetris/src/GameLogic.cpp
+++ examples/tetris/src/GameLogic.cpp
@@ -46,43 +46,26 @@ GameLogic::GameLogic(
     //scene->setRootObject(root);
 
     timersProcessor->createTimer(1000 * 1000 * 1)->addHandler([this] {
-        for (auto tetromino : tetrominos) {
-            for (auto child : tetromino->getChildren()) {
-                if (auto movableChild
-                    = std::dynamic_pointer_cast<MovableGameObject<nDimensions, BaseType>>(child)) {
+        this->handleTimer();
+    });
 
-                    auto position = movableChild->getPosition();
-                    game_math::Vector<2, int> positionInMap = position;
-                    positionInMap[0] /= blockSize;
-                    positionInMap[1] /= blockSize;
+    timersProcessor->createTimer(1000 * 1000 * 5)->addHandler([this] {
+        auto shape = Matrix<4, 4, bool>(
+            { { 1, 0, 0, 0 },
+                { 1, 0, 0, 0 },
+                { 1, 0, 0, 0 },
+                { 1, 0, 0, 0 } });
 
-                    if (positionInMap[0] >= 0 && positionInMap[1] > 0) {
-                        if (this->sceneMap->getData()
-                                [static_cast<size_t>(positionInMap[1] - 1)]
-                                [static_cast<size_t>(positionInMap[0])]
-                                    .size()
-                            == 0) {
+        BaseType x = 0;
+        BaseType y = this->scene->getSize()[1] - blockSize * 4;
 
-                            position[1] -= blockSize;
-                        }
-                    }
-
-                    /*if (position[1] < 0) {
-                        position[1] = 0;
-                    }*/
-
-                    movableChild->setPosition(position);
-                }
-            }
-        }
-
-        for (size_t row = 0; row < sceneRows; ++row) {
-            for (size_t column = 0; column < sceneColumns; ++column) {
-                std::cout << this->sceneMap->getData()[row][column].size() << " ";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
+        auto tetromino = std::shared_ptr<Tetromino>(new Tetromino(
+            { x, y },
+            blockSize,
+            { 255, 0, 0 },
+            shape));
+        tetrominos.insert(tetromino);
+        this->scene->getRootObject()->addChild(tetromino);
     });
 }
 
@@ -90,17 +73,66 @@ void GameLogic::onModuleCreation()
 {
 }
 
-void GameLogic::processFrame(int32_t deltaTime)
+void GameLogic::processFrame(int32_t)
 {
-    /*for (std::shared_ptr<Tetromino> tetromino1 : tetrominos) {
-        for (std::shared_ptr<GameObject<2, BaseType>> tetromino2 : tetromino1->getChildren()) {
-            if (auto tetromino
-                = std::dynamic_pointer_cast<MovableGameObject<nDimensions, BaseType>>(tetromino2)) {
+}
 
-                auto& positionRef = tetromino->getPositionRef();
+void GameLogic::handleTimer()
+{
+    for (auto tetromino : tetrominos) {
+        bool canBeMoved = true;
 
-                positionRef[1] -= tetrominoSpeed; // * float(deltaTime) / 1000000 * blockSize;
+        for (auto child : tetromino->getChildren()) {
+            if (auto movableChild
+                = std::dynamic_pointer_cast<MovableGameObject<nDimensions, BaseType>>(child)) {
+
+                auto position = movableChild->getPosition();
+                game_math::Vector<2, int> positionInMap = position;
+                positionInMap[0] /= blockSize;
+                positionInMap[1] /= blockSize;
+
+                if (positionInMap[0] < 0 || positionInMap[1] <= 0) {
+                    canBeMoved = false;
+                    break;
+                }
+
+                if (positionInMap[0] >= 0 && positionInMap[1] > 0) {
+                    auto& cell = this->sceneMap->getData()
+                                     [static_cast<size_t>(positionInMap[1] - 1)]
+                                     [static_cast<size_t>(positionInMap[0])];
+
+                    for (const auto& item : cell) {
+                        if (item->getParent() != tetromino.get()) {
+                            canBeMoved = false;
+                            break;
+                        }
+                    }
+                }
+
+                // movableChild->setPosition(position);
             }
         }
-    }*/
+
+        if (canBeMoved) {
+            for (auto child : tetromino->getChildren()) {
+                if (auto movableChild
+                    = std::dynamic_pointer_cast<MovableGameObject<nDimensions, BaseType>>(child)) {
+
+                    auto position = movableChild->getPosition();
+
+                    position[1] -= blockSize;
+
+                    movableChild->setPosition(position);
+                }
+            }
+        }
+    }
+
+    for (size_t row = 0; row < sceneRows; ++row) {
+        for (size_t column = 0; column < sceneColumns; ++column) {
+            std::cout << this->sceneMap->getData()[row][column].size() << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
 }
--- examples/tetris/src/GameLogic.h
+++ examples/tetris/src/GameLogic.h
@@ -21,6 +21,8 @@ public:
     virtual void processFrame(int32_t deltaTime) override;
 
 private:
+    void handleTimer();
+
     std::shared_ptr<TetrisScene> scene;
     std::shared_ptr<Scene2Map<BaseType, sceneRows, sceneColumns>> sceneMap;
     std::shared_ptr<TimersProcessor> timersProcessor;
